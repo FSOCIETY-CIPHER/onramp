9a4dc136bccf0ba894b32d18d64ea65f98ca5cf4
unknown <afolabihectorodunsi@gmail.com>
2022-11-06 08:07:27 +0100

    added else to payment

--- payment.js
+++ payment.js
@@ -2,5 +2,7 @@ let newPayment = 9000;
 let oldPayment = 5000;
 
 if (newPayment > oldPayment) {
-    console.log(`You have a balance of ${newPayment -oldPayment}`)
+    console.log(`You have a balance of ${newPayment -oldPayment}`);
+} else{
+    console.log(`You have no balance i your account`)
 }
